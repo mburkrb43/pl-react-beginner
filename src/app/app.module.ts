f18a18ef77a5a0a58b6f5eeead087d62c6a66615
Michael Burkhart <mburkhart41@gmail.com>
2021-02-07 20:16:08 -0500

    Shared refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent} from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
-import { ProductModule } from './products/product.module'
+import { ProductModule } from './products/product.module';
 @NgModule({
   declarations: [
     AppComponent,
